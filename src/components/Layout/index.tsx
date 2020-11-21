1f2774ab4468fc73529479117b714df27f2c3703
janihyvo <jantsalol@hotmail.com>
2020-11-21 13:34:30 +0200

    Import & export every component to and from index.ts for future quality of life reasons

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 
-import Header from "../Header";
-import Footer from "../Footer";
+import { Header, Footer } from "../index"
+
 
 const useStyles = makeStyles({
   main: {
