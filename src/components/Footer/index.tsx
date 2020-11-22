bab474f36922df7806cab8e5b54768a41f9c9146
roopertti <robert.kuhlmann2@gmail.com>
2020-11-22 12:18:53 +0200

    style: some prettier and eslint style fixes

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -14,7 +14,7 @@ const useStyles = makeStyles({
     justifyContent: "center",
     alignItems: "center",
     display: "flex",
-  }
+  },
 });
 
 const Footer = () => {
@@ -25,10 +25,14 @@ const Footer = () => {
     <footer className={classes.footer}>
       <Grid className={classes.container} container>
         <Grid>
-          <VideoButtons isPlaying={isWatching} play={() => setIsWatching(!isWatching)} skipNext={() => {}} skipPrev={() => {}} />
+          <VideoButtons
+            isPlaying={isWatching}
+            play={() => setIsWatching(!isWatching)}
+            skipNext={() => {}}
+            skipPrev={() => {}}
+          />
         </Grid>
       </Grid>
-      
     </footer>
   );
 };
